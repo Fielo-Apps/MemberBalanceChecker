7ac25c8f498915b50eb121936671db3f02771dbb
Tiago Leal <tibeal@gmail.com>
2023-07-20 17:58:47 -0300

    new stuff

---
+++ scripts/shell/uploadPromoteInstall.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+versionNumber="1.8.0"
+
+sf package version create --package 0HoHY000000000u0AA -n "${versionNumber}.1" --installation-key-bypass --code-coverage -w 120 &&
+
+sf package version promote --package "FieloMBC@${versionNumber}-1" -v dev-hub-infra --no-prompt &&
+
+sfdx force:package:install --package "FieloMBC@${versionNumber}-1" --target-org terpel-prod -w 120
